2a67d7ba15a69447950499c01c861921b08a9f23
hakomori64 <hakomori64@gmail.com>
2021-08-17 15:44:40 +0900

    implement parent keyboard

--- sound_keyboard/keyboard/keyboard.py
+++ sound_keyboard/keyboard/keyboard.py
@@ -1,5 +1,10 @@
 import pygame
 import sys
+import os
+from sound_keyboard.keyboard.state_controller import (
+    KeyboardStateController,
+    Direction
+)
 
 # constants
 BACKGROUND_COLOR = (242, 242, 242)
@@ -11,12 +16,13 @@ class Keyboard:
     def __init__(self):
         # initialize app
         pygame.init()
+        pygame.display.set_caption('Smile!')
 
         # setting initial window size and set window resizable
         self.surface = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
 
-        # set caption
-        pygame.display.set_caption('Smile!')
+        # setting keyboard controller
+        self.keyboard_state_controller = KeyboardStateController()
 
 
     def start(self):
@@ -26,7 +32,7 @@ class Keyboard:
             self.surface.fill(BACKGROUND_COLOR)
 
             font = pygame.font.Font(FONT_PATH, int(min(self.surface.get_width(), self.surface.get_height()) / 10))
-            text = font.render('あ', True, FONT_COLOR, None)
+            text = font.render(self.keyboard_state_controller.current_parent_char, True, FONT_COLOR, None)
             textRect = text.get_rect()
             textRect.center = (self.surface.get_width() / 2, self.surface.get_height() / 2)
 
@@ -43,6 +49,19 @@ class Keyboard:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
+                
+                #TODO(hakomori64) replace current char using gesture
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.keyboard_state_controller.move(Direction.LEFT)
+                    if event.key == pygame.K_UP:
+                        self.keyboard_state_controller.move(Direction.UP)
+                    if event.key == pygame.K_RIGHT:
+                        self.keyboard_state_controller.move(Direction.RIGHT)
+                    if event.key == pygame.K_DOWN:
+                        self.keyboard_state_controller.move(Direction.DOWN)
+                    
 
 if __name__ == '__main__':
+    print (os.getcwd())
     Keyboard().start()
